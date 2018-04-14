03bd72ba05f6ac1741e9e1c3a6f7b271a9eb1949
duquedd1@gmail.com Duque <duquedd1@gmail.com>
2018-04-14 19:30:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function shout(string){
   return string.toUpperCase()
 }
+function whisper(string){
+  return string.lowercase()
+}
+function logShout(string){
+  console.log(string.uppercase())
+}
+function logWhisper(string){
+  console.log(string.lowercase())
+}
